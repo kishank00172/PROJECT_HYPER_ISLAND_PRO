bcccbf2c98105bd559781f8be28a786b41545595
kishank00172 <149560770+kishank00172@users.noreply.github.com>
2026-05-23 19:04:16 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,10 @@ android {
         versionName = "0.0.1-phase0"
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
