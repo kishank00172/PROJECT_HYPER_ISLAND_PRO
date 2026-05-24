e2a30ee3688893203a92927543439d12142ee6ac
kishank00172 <149560770+kishank00172@users.noreply.github.com>
2026-05-24 10:09:45 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.1-phase0"
+        versionName = "0.2.0-phase2"
     }
 
     buildFeatures {
